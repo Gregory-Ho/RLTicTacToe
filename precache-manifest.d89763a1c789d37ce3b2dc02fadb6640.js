6c78440e178dec90cf940e6f1b378bfd33d96d42
BuildTools <unconfigured@null.spigotmc.org>
2020-09-10 00:48:51 -0400

    Update Build Files

---
+++ precache-manifest.d89763a1c789d37ce3b2dc02fadb6640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccba89528223754d2572632a781fa61c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b689bb379e0972fd9f7a",
+    "url": "./static/css/main.c7c8fff5.chunk.css"
+  },
+  {
+    "revision": "ebc64b6df253b099b860",
+    "url": "./static/js/2.2c92b97b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2c92b97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b689bb379e0972fd9f7a",
+    "url": "./static/js/main.6469031a.chunk.js"
+  },
+  {
+    "revision": "d44f0fcb62de99ba320f",
+    "url": "./static/js/runtime-main.8d9b9726.js"
+  }
+]);
